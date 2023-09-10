77b2d0c05ff376d7c635e4589ca340c9997b7694
Abidur Rahman Arko <aroarko.sd@gmail.com>
2023-09-11 01:16:06 +0600

    Header file

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <h1>Header</h1>
+            <a href="1">1</a>
+            <a href="2">2</a>
+            <a href="3">3</a>
+            <a href="4">4</a> 
+        </div>
+    );
+};
+
+export default Header;
